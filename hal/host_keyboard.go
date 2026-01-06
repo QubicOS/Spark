27efacf0c7526ae3920f9f4d1bf01a67a20190da
dm-vev <carmakdoom@gmail.com>
2026-01-06 20:30:46 +0300

    termkbd: stop repeat on key release

--- hal/host_keyboard.go
+++ hal/host_keyboard.go
@@ -17,9 +17,9 @@ func newHostKeyboard() *hostKeyboard {
 func (k *hostKeyboard) Events() <-chan KeyEvent { return k.ch }
 
 func (k *hostKeyboard) poll() {
-	emit := func(code KeyCode) {
+	emit := func(code KeyCode, press bool) {
 		select {
-		case k.ch <- KeyEvent{Code: code, Press: true}:
+		case k.ch <- KeyEvent{Code: code, Press: press}:
 		default:
 		}
 	}
@@ -58,36 +58,69 @@ func (k *hostKeyboard) poll() {
 
 	// Use only arrow keys for navigation. Letter keys are treated as text input.
 	if inpututil.IsKeyJustPressed(ebiten.KeyArrowUp) {
-		emit(KeyUp)
+		emit(KeyUp, true)
+	}
+	if inpututil.IsKeyJustReleased(ebiten.KeyArrowUp) {
+		emit(KeyUp, false)
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyArrowDown) {
-		emit(KeyDown)
+		emit(KeyDown, true)
+	}
+	if inpututil.IsKeyJustReleased(ebiten.KeyArrowDown) {
+		emit(KeyDown, false)
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyArrowLeft) {
-		emit(KeyLeft)
+		emit(KeyLeft, true)
+	}
+	if inpututil.IsKeyJustReleased(ebiten.KeyArrowLeft) {
+		emit(KeyLeft, false)
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyArrowRight) {
-		emit(KeyRight)
+		emit(KeyRight, true)
+	}
+	if inpututil.IsKeyJustReleased(ebiten.KeyArrowRight) {
+		emit(KeyRight, false)
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
-		emit(KeyEnter)
+		emit(KeyEnter, true)
+	}
+	if inpututil.IsKeyJustReleased(ebiten.KeyEnter) {
+		emit(KeyEnter, false)
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
-		emit(KeyEscape)
+		emit(KeyEscape, true)
+	}
+	if inpututil.IsKeyJustReleased(ebiten.KeyEscape) {
+		emit(KeyEscape, false)
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyBackspace) {
-		emit(KeyBackspace)
+		emit(KeyBackspace, true)
+	}
+	if inpututil.IsKeyJustReleased(ebiten.KeyBackspace) {
+		emit(KeyBackspace, false)
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyTab) {
-		emit(KeyTab)
+		emit(KeyTab, true)
+	}
+	if inpututil.IsKeyJustReleased(ebiten.KeyTab) {
+		emit(KeyTab, false)
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyDelete) {
-		emit(KeyDelete)
+		emit(KeyDelete, true)
+	}
+	if inpututil.IsKeyJustReleased(ebiten.KeyDelete) {
+		emit(KeyDelete, false)
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyHome) {
-		emit(KeyHome)
+		emit(KeyHome, true)
+	}
+	if inpututil.IsKeyJustReleased(ebiten.KeyHome) {
+		emit(KeyHome, false)
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyEnd) {
-		emit(KeyEnd)
+		emit(KeyEnd, true)
+	}
+	if inpututil.IsKeyJustReleased(ebiten.KeyEnd) {
+		emit(KeyEnd, false)
 	}
 }
