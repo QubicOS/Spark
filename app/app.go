371d67bb3991c395ff82624bb399f6d56c3c2954
dm-vev <carmakdoom@gmail.com>
2026-01-06 12:09:50 +0300

    Wire term service and demo into app config

--- app/app.go
+++ app/app.go
@@ -4,17 +4,23 @@ import (
 	"spark/hal"
 	"spark/sparkos/kernel"
 	"spark/sparkos/services/logger"
+	"spark/sparkos/services/term"
 	timesvc "spark/sparkos/services/time"
 	"spark/sparkos/services/ui"
+	"spark/sparkos/tasks/termdemo"
 )
 
 type system struct {
 	k *kernel.Kernel
 }
 
-// New initializes and starts the OS.
+type Config struct {
+	TermDemo bool
+}
+
+// New initializes and starts the OS with default config.
 func New(h hal.HAL) func() error {
-	_ = newSystem(h)
+	_ = newSystem(h, Config{})
 	return func() error { return nil }
 }
 
@@ -24,15 +30,32 @@ func Run(h hal.HAL) {
 	select {}
 }
 
-func newSystem(h hal.HAL) *system {
+func NewWithConfig(h hal.HAL, cfg Config) func() error {
+	_ = newSystem(h, cfg)
+	return func() error { return nil }
+}
+
+func RunWithConfig(h hal.HAL, cfg Config) {
+	_ = NewWithConfig(h, cfg)
+	select {}
+}
+
+func newSystem(h hal.HAL, cfg Config) *system {
 	k := kernel.New()
 
 	logEP := k.NewEndpoint(kernel.RightSend | kernel.RightRecv)
 	timeEP := k.NewEndpoint(kernel.RightSend | kernel.RightRecv)
+	termEP := k.NewEndpoint(kernel.RightSend | kernel.RightRecv)
 
 	k.AddTask(logger.New(h.Logger(), logEP.Restrict(kernel.RightRecv)))
 	k.AddTask(timesvc.New(timeEP))
-	k.AddTask(ui.New(h.Display(), h.Input()))
+
+	if cfg.TermDemo {
+		k.AddTask(term.New(h.Display(), termEP.Restrict(kernel.RightRecv)))
+		k.AddTask(termdemo.New(timeEP.Restrict(kernel.RightSend), termEP.Restrict(kernel.RightSend)))
+	} else {
+		k.AddTask(ui.New(h.Display(), h.Input()))
+	}
 
 	if ht := h.Time(); ht != nil {
 		if ch := ht.Ticks(); ch != nil {
