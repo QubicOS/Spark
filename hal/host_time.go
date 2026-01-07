3d436ed1e0fc79fb6d3e2fb030992a753d208b8a
dm-vev <carmakdoom@gmail.com>
2026-01-07 15:04:40 +0300

    hal: make host ticks time-based

--- hal/host_time.go
+++ hal/host_time.go
@@ -2,9 +2,14 @@
 
 package hal
 
+import "time"
+
 type hostTime struct {
 	ch  chan uint64
 	seq uint64
+
+	last time.Time
+	acc  time.Duration
 }
 
 func newHostTime() *hostTime {
@@ -14,6 +19,27 @@ func newHostTime() *hostTime {
 func (t *hostTime) Ticks() <-chan uint64 { return t.ch }
 
 func (t *hostTime) step(n uint64) {
+	now := time.Now()
+	if t.last.IsZero() {
+		t.last = now
+		t.acc = 0
+		t.stepN(n)
+		return
+	}
+
+	t.acc += now.Sub(t.last)
+	t.last = now
+
+	const tickDur = time.Millisecond
+	ticks := uint64(t.acc / tickDur)
+	if ticks == 0 {
+		return
+	}
+	t.acc = t.acc % tickDur
+	t.stepN(ticks)
+}
+
+func (t *hostTime) stepN(n uint64) {
 	for i := uint64(0); i < n; i++ {
 		t.seq++
 		select {
@@ -22,4 +48,3 @@ func (t *hostTime) step(n uint64) {
 		}
 	}
 }
-
