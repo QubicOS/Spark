16cdd19bbb4547ddefda081549378f5280a544ca
dm-vev <carmakdoom@gmail.com>
2026-01-07 10:35:27 +0300

    Extend app manager to more apps

--- sparkos/services/appmgr/service.go
+++ sparkos/services/appmgr/service.go
@@ -4,7 +4,11 @@ import (
 	"spark/hal"
 	"spark/sparkos/kernel"
 	"spark/sparkos/proto"
+	hexedittask "spark/sparkos/tasks/hexedit"
+	imgviewtask "spark/sparkos/tasks/imgview"
 	mctask "spark/sparkos/tasks/mc"
+	rtdemotask "spark/sparkos/tasks/rtdemo"
+	rtvoxeltask "spark/sparkos/tasks/rtvoxel"
 	vectortask "spark/sparkos/tasks/vector"
 	vitask "spark/sparkos/tasks/vi"
 )
@@ -13,40 +17,72 @@ type Service struct {
 	disp   hal.Display
 	vfsCap kernel.Capability
 
-	viProxyCap     kernel.Capability
-	mcProxyCap     kernel.Capability
-	vectorProxyCap kernel.Capability
+	rtdemoProxyCap  kernel.Capability
+	rtvoxelProxyCap kernel.Capability
+	imgviewProxyCap kernel.Capability
+	hexProxyCap     kernel.Capability
+	viProxyCap      kernel.Capability
+	mcProxyCap      kernel.Capability
+	vectorProxyCap  kernel.Capability
 
-	viCap     kernel.Capability
-	mcCap     kernel.Capability
-	vectorCap kernel.Capability
+	rtdemoCap  kernel.Capability
+	rtvoxelCap kernel.Capability
+	imgviewCap kernel.Capability
+	hexCap     kernel.Capability
+	viCap      kernel.Capability
+	mcCap      kernel.Capability
+	vectorCap  kernel.Capability
 
-	viEP     kernel.Capability
-	mcEP     kernel.Capability
-	vectorEP kernel.Capability
+	rtdemoEP  kernel.Capability
+	rtvoxelEP kernel.Capability
+	imgviewEP kernel.Capability
+	hexEP     kernel.Capability
+	viEP      kernel.Capability
+	mcEP      kernel.Capability
+	vectorEP  kernel.Capability
 
-	viRunning     bool
-	mcRunning     bool
-	vectorRunning bool
+	rtdemoRunning  bool
+	rtvoxelRunning bool
+	imgviewRunning bool
+	hexRunning     bool
+	viRunning      bool
+	mcRunning      bool
+	vectorRunning  bool
 }
 
-func New(disp hal.Display, vfsCap, viProxyCap, mcProxyCap, vectorProxyCap, viCap, mcCap, vectorCap, viEP, mcEP, vectorEP kernel.Capability) *Service {
+func New(disp hal.Display, vfsCap, rtdemoProxyCap, rtvoxelProxyCap, imgviewProxyCap, hexProxyCap, viProxyCap, mcProxyCap, vectorProxyCap, rtdemoCap, rtvoxelCap, imgviewCap, hexCap, viCap, mcCap, vectorCap, rtdemoEP, rtvoxelEP, imgviewEP, hexEP, viEP, mcEP, vectorEP kernel.Capability) *Service {
 	return &Service{
-		disp:           disp,
-		vfsCap:         vfsCap,
-		viProxyCap:     viProxyCap,
-		mcProxyCap:     mcProxyCap,
-		vectorProxyCap: vectorProxyCap,
-		viCap:          viCap,
-		mcCap:          mcCap,
-		vectorCap:      vectorCap,
-		viEP:           viEP,
-		mcEP:           mcEP,
-		vectorEP:       vectorEP,
+		disp:            disp,
+		vfsCap:          vfsCap,
+		rtdemoProxyCap:  rtdemoProxyCap,
+		rtvoxelProxyCap: rtvoxelProxyCap,
+		imgviewProxyCap: imgviewProxyCap,
+		hexProxyCap:     hexProxyCap,
+		viProxyCap:      viProxyCap,
+		mcProxyCap:      mcProxyCap,
+		vectorProxyCap:  vectorProxyCap,
+		rtdemoCap:       rtdemoCap,
+		rtvoxelCap:      rtvoxelCap,
+		imgviewCap:      imgviewCap,
+		hexCap:          hexCap,
+		viCap:           viCap,
+		mcCap:           mcCap,
+		vectorCap:       vectorCap,
+		rtdemoEP:        rtdemoEP,
+		rtvoxelEP:       rtvoxelEP,
+		imgviewEP:       imgviewEP,
+		hexEP:           hexEP,
+		viEP:            viEP,
+		mcEP:            mcEP,
+		vectorEP:        vectorEP,
 	}
 }
 
 func (s *Service) Run(ctx *kernel.Context) {
+	go s.runProxy(ctx, s.rtdemoProxyCap, proto.AppRTDemo)
+	go s.runProxy(ctx, s.rtvoxelProxyCap, proto.AppRTVoxel)
+	go s.runProxy(ctx, s.imgviewProxyCap, proto.AppImgView)
+	go s.runProxy(ctx, s.hexProxyCap, proto.AppHex)
 	go s.runProxy(ctx, s.viProxyCap, proto.AppVi)
 	go s.runProxy(ctx, s.mcProxyCap, proto.AppMC)
 	go s.runProxy(ctx, s.vectorProxyCap, proto.AppVector)
@@ -75,7 +111,9 @@ func (s *Service) runProxy(ctx *kernel.Context, proxyCap kernel.Capability, appI
 				continue
 			}
 
-			_ = ctx.SendToCapResult(s.appCapByID(appID), msg.Kind, msg.Data[:msg.Len], kernel.Capability{})
+			if s.isRunning(appID) {
+				_ = ctx.SendToCapResult(s.appCapByID(appID), msg.Kind, msg.Data[:msg.Len], kernel.Capability{})
+			}
 			s.stop(ctx, appID)
 
 		case proto.MsgTermInput:
@@ -87,6 +125,34 @@ func (s *Service) runProxy(ctx *kernel.Context, proxyCap kernel.Capability, appI
 
 func (s *Service) ensureRunning(ctx *kernel.Context, appID proto.AppID) {
 	switch appID {
+	case proto.AppRTDemo:
+		if s.rtdemoRunning {
+			return
+		}
+		ctx.AddTask(rtdemotask.New(s.disp, s.rtdemoEP))
+		s.rtdemoRunning = true
+
+	case proto.AppRTVoxel:
+		if s.rtvoxelRunning {
+			return
+		}
+		ctx.AddTask(rtvoxeltask.New(s.disp, s.rtvoxelEP))
+		s.rtvoxelRunning = true
+
+	case proto.AppImgView:
+		if s.imgviewRunning {
+			return
+		}
+		ctx.AddTask(imgviewtask.New(s.disp, s.imgviewEP, s.vfsCap))
+		s.imgviewRunning = true
+
+	case proto.AppHex:
+		if s.hexRunning {
+			return
+		}
+		ctx.AddTask(hexedittask.New(s.disp, s.hexEP, s.vfsCap))
+		s.hexRunning = true
+
 	case proto.AppVi:
 		if s.viRunning {
 			return
@@ -112,6 +178,34 @@ func (s *Service) ensureRunning(ctx *kernel.Context, appID proto.AppID) {
 
 func (s *Service) stop(ctx *kernel.Context, appID proto.AppID) {
 	switch appID {
+	case proto.AppRTDemo:
+		if !s.rtdemoRunning {
+			return
+		}
+		s.rtdemoRunning = false
+		_ = ctx.SendToCapResult(s.rtdemoCap, uint16(proto.MsgAppShutdown), nil, kernel.Capability{})
+
+	case proto.AppRTVoxel:
+		if !s.rtvoxelRunning {
+			return
+		}
+		s.rtvoxelRunning = false
+		_ = ctx.SendToCapResult(s.rtvoxelCap, uint16(proto.MsgAppShutdown), nil, kernel.Capability{})
+
+	case proto.AppImgView:
+		if !s.imgviewRunning {
+			return
+		}
+		s.imgviewRunning = false
+		_ = ctx.SendToCapResult(s.imgviewCap, uint16(proto.MsgAppShutdown), nil, kernel.Capability{})
+
+	case proto.AppHex:
+		if !s.hexRunning {
+			return
+		}
+		s.hexRunning = false
+		_ = ctx.SendToCapResult(s.hexCap, uint16(proto.MsgAppShutdown), nil, kernel.Capability{})
+
 	case proto.AppVi:
 		if !s.viRunning {
 			return
@@ -137,6 +231,14 @@ func (s *Service) stop(ctx *kernel.Context, appID proto.AppID) {
 
 func (s *Service) appCapByID(appID proto.AppID) kernel.Capability {
 	switch appID {
+	case proto.AppRTDemo:
+		return s.rtdemoCap
+	case proto.AppRTVoxel:
+		return s.rtvoxelCap
+	case proto.AppImgView:
+		return s.imgviewCap
+	case proto.AppHex:
+		return s.hexCap
 	case proto.AppVi:
 		return s.viCap
 	case proto.AppMC:
@@ -145,5 +247,26 @@ func (s *Service) appCapByID(appID proto.AppID) kernel.Capability {
 		return s.vectorCap
 	default:
 		return kernel.Capability{}
+	}
+}
+
+func (s *Service) isRunning(appID proto.AppID) bool {
+	switch appID {
+	case proto.AppRTDemo:
+		return s.rtdemoRunning
+	case proto.AppRTVoxel:
+		return s.rtvoxelRunning
+	case proto.AppImgView:
+		return s.imgviewRunning
+	case proto.AppHex:
+		return s.hexRunning
+	case proto.AppVi:
+		return s.viRunning
+	case proto.AppMC:
+		return s.mcRunning
+	case proto.AppVector:
+		return s.vectorRunning
+	default:
+		return false
 	}
 }
